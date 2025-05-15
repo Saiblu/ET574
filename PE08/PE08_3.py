5cdb2be5cdb5ef699ac7d2a46cc114de09316525
Eli <103086206+Saiblu@users.noreply.github.com>
2025-05-15 11:35:56 -0400

    Delete PE08 directory

--- PE08/PE08_3.py
+++
@@ -1,6 +0,0 @@
-dict1 = {'Ten': 10, 'Twenty': 20, 'Thirty': 30} #This is a dictionary with keys and values.
-dict2 = {'Thirty': 30, 'Forty': 40, 'Fifty': 50}#This is a dictionary with keys and values.
-merged_dict = {**dict1, **dict2} #This will merge both dictionaries into one dictionary.
-print(merged_dict) #Will print the merged dictionary.
-#Output will be {'Ten': 10, 'Twenty': 20, 'Thirty': 30, 'Forty': 40, 'Fifty': 50}
-
